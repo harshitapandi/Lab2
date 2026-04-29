3d1408cfc477baadd6d124859f19f1aa365ec258
harshitapandi <pharshita.25@ichat.sp.edu.sg>
2026-04-29 11:32:21 +0800

    BMI calculator

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,17 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python!")
+
+def calculate_bmi(height, weight):
+    print("Height =" + str(height))
+    print("Weight =" + str(weight))
+
+    bmi = weight / (height * height)
+    print("BMI = " + str(bmi))
+
+    if (bmi < 18.5):
+        print("Underweight")
+    elif (bmi >= 18.5 and bmi < 25):
+        print("Normal weight")
+    else:
+        print("Overweight")
+
+calculate_bmi(weight=47, height=1.58)
