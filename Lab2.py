323ba15bf7a9e0788dcdbf8f611a0759f0b1d813
harshitapandi <pharshita.25@ichat.sp.edu.sg>
2026-05-01 17:33:57 +0800

    updated

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,10 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python!")
+
+def calculate_bmi(height, weight):
+    print("Height =" + str(height))
+    print("Weight =" + str(weight))
+
+    bmi = weight / (height ** 2)
+    print("BMI = " + str(bmi))
+
+calculate_bmi(height=1.73, weight=57)
